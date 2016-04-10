3b6c348aa42e1f15db154ee2e3d4fa169eb39964
Justas Trimailovas <j.trimailovas@gmail.com>
2016-04-10 17:23:15 +0300

    Use grouped channels

--- canal/routing.py
+++ canal/routing.py
@@ -1,6 +1,9 @@
 from channels.routing import route
+from app.consumers import ws_message, ws_add, ws_disconnect
 
 
 channel_routing = [
-    route("http.request", "app.consumers.http_consumer"),
+    route("websocket.receive", ws_message),
+    route("websocket.connect", ws_add),
+    route("websocket.disconnect", ws_disconnect),
 ]
