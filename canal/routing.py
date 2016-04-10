625610869c3796727e1216f52b81b42c67908aed
Justas Trimailovas <j.trimailovas@gmail.com>
2016-04-10 18:33:15 +0300

    Persistant data

--- canal/routing.py
+++ canal/routing.py
@@ -1,9 +1,9 @@
 from channels.routing import route
-from app.consumers import ws_message, ws_add, ws_disconnect
+from app.consumers import ws_connect, ws_message, ws_disconnect
 
 
 channel_routing = [
+    route("websocket.connect", ws_connect),
     route("websocket.receive", ws_message),
-    route("websocket.connect", ws_add),
     route("websocket.disconnect", ws_disconnect),
 ]
