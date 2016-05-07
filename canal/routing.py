9353235ec0cc68077131bd97dcbd04582ad79f57
Justas Trimailovas <j.trimailovas@gmail.com>
2016-05-07 09:00:51 +0300

    Put message websocket consumers under '/chat' path

--- canal/routing.py
+++ canal/routing.py
@@ -1,10 +1,16 @@
+from channels import include
 from channels.routing import route
 from canal.consumers import ws_connect, ws_message, ws_disconnect, msg_consumer
 
 
-channel_routing = [
-    route("chat-messages", msg_consumer),
+message_routing = [
     route("websocket.connect", ws_connect),
     route("websocket.receive", ws_message),
     route("websocket.disconnect", ws_disconnect),
+]
+
+
+channel_routing = [
+    route("chat-messages", msg_consumer),
+    include("canal.routing.message_routing", path=r"^/chat"),
 ]
