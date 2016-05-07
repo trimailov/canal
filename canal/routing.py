71692d9c26877d64e5f7a724c7aefad82ffec5fd
Justas Trimailovas <j.trimailovas@gmail.com>
2016-05-07 09:30:03 +0300

    Use multiple websockets on same page

--- canal/routing.py
+++ canal/routing.py
@@ -1,16 +1,25 @@
 from channels import include
 from channels.routing import route
-from canal.consumers import ws_connect, ws_message, ws_disconnect, msg_consumer
+from canal import consumers
 
 
 message_routing = [
-    route("websocket.connect", ws_connect),
-    route("websocket.receive", ws_message),
-    route("websocket.disconnect", ws_disconnect),
+    route("websocket.connect", consumers.ws_connect),
+    route("websocket.receive", consumers.ws_message),
+    route("websocket.disconnect", consumers.ws_disconnect),
+]
+
+
+long_task_routing = [
+    route("websocket.connect", consumers.ws_connect),
+    route("websocket.receive", consumers.ws_message),
+    route("websocket.disconnect", consumers.ws_disconnect),
 ]
 
 
 channel_routing = [
-    route("chat-messages", msg_consumer),
+    route("chat-messages", consumers.msg_consumer),
+
     include("canal.routing.message_routing", path=r"^/chat"),
+    include("canal.routing.long_task_routing", path=r"^/second-chat"),
 ]
